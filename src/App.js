05511542ad377828738d5e3503f23d00d0a84e61
raghav_1482 <raghvendra1482002@gmail.com>
2023-10-11 01:17:32 +0530

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navb from './components/Navbar';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Foot from './components/foot';
 import About from './components/about';
@@ -17,27 +17,27 @@ import Home from './components/Home';
 
 function App() {
 
-  const [mydat,setMyDat]=useState([])
-  useEffect(()=>{
-      axios.get("https://api.tvmaze.com/shows")
-      .then((res)=>setMyDat(res.data));
-    },[]);
-    var i=1;
+  const [mydat, setMyDat] = useState([])
+  useEffect(() => {
+    axios.get("https://api.tvmaze.com/shows")
+      .then((res) => setMyDat(res.data));
+  }, []);
+  var i = 1;
   return (
     <div className="App">
-        <Navb/>
-        <Router>
-          <Routes>
-            <Route exact path={`/`} element={<Home/>}></Route>
-          </Routes>
-        {mydat.map(dat=>{
-                return <Routes><Route exact path={`/Moviedesc${dat.id}`} element={<Moviedesc title={dat.name} page={dat.url} keys={dat.id} imag={dat.image.original} detail={dat.summary} num={i-1}/>}></Route></Routes>
-            })};
-        <Routes><Route  path={`/about`} element={<About/>}></Route></Routes>
-        <Routes><Route path={`/contact`} element={<Contact/>}></Route></Routes>
-        </Router>
-        <div className='contain1'></div>
-        <Foot/>
+      <Router>
+      <Navb />
+        <Routes>
+          <Route exact path={`/`} element={<Home />}></Route>
+          {mydat.map(dat => {
+            return <Route exact path={`/Moviedesc${dat.id}`} element={<Moviedesc title={dat.name} page={dat.url} keys={dat.id} imag={dat.image.original} detail={dat.summary} num={i - 1} />}></Route>
+          })};
+          <Route path={`/about`} element={<About />}></Route>
+          <Route path={`/contact`} element={<Contact />}></Route>
+        </Routes>
+      <Foot />
+      </Router>
+      {/* <div className='contain1'></div> */}
     </div>
   );
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -3,18 +3,18 @@ import "../App.css";
 
 export default function Contact(){
     return(<>
-    <div className="container1" style={{color:"white"}}>
+    <div className="container1" style={{color:"white" , width:"60%" , margin:"auto"}}>
   <form action="mailto:recipient@example.com" method="post" enctype="text/plain">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" for="fname" placeholder="Your name.." style={{marginBottom:"20px"}}/>
+    <div style={{display:"flex" , margin:"auto" , justifyContent:"space-between" , width:"100%"}}><label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" for="fname" placeholder="Your name.." style={{marginBottom:"20px"}}/></div>
 
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" for="lname" placeholder="Your last name.." style={{marginBottom:"20px"}}/>
+    <div style={{display:"flex" , margin:"auto" , justifyContent:"space-between" , width:"100%"}}><label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" for="lname" placeholder="Your last name.." style={{marginBottom:"20px"}}/></div>
 
-    <label for="message">Message</label><br/>
-    <textarea id="message" name="message" for="message" placeholder="Write something.." style={{height:"100px" , width:"100%" , marginBottom:"20px"}}></textarea><br/>
+    <div style={{display:"flex" , margin:"auto" , justifyContent:"space-between" , width:"100%"}}><label for="message">Message</label><br/>
+    <textarea id="message" name="message" for="message" placeholder="Write something.." style={{height:"100px" , width:"40%" , margin:"auto"}}></textarea><br/></div>
 
-    <input type="submit" style={{backgroundColor:"rgb(206, 27, 27)" , color:"white"  , margin:"auto"}} value="Submit"/>
+    <input type="submit" style={{backgroundColor:"rgb(206, 27, 27)" , color:"white"  , margin:"40px auto" , width:"130px" , height:"40px"}} value="Submit"/>
   </form>
 </div>
     </>);
